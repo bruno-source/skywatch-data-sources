b70da1806515e21d8ffd5323cfcb5de0cbda202f
bruno-source <cardosobrunob+2@gmail.com>
2019-11-27 23:17:17 -0300

    Add data sources parser

--- skywatch_data_sources/sources.py
+++ skywatch_data_sources/sources.py
@@ -1,2 +1,32 @@
+from operator import itemgetter
+
+import requests
+from bs4 import BeautifulSoup
+
+
 def get_data_sources():
-    pass
+    """
+    Parses www.skywatch.com and returns a list of its available data sources
+    """
+    url = 'https://www.skywatch.com/sitemap.xml'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    images = soup.find_all('image:image')
+
+    sources = []
+    for image in images:
+        if image.find('image:title').get_text() == 'Sources':
+            source, resolution, _ = image.find(
+                'image:caption').get_text().split(' / ')
+            splited_source = source.split(' Provider: ')
+
+            data = {
+                'Source': splited_source[0].strip(),
+                'Provider': splited_source[1].strip(),
+                'Resolution': resolution.split(': ')[1].strip(),
+            }
+
+            sources.append(data)
+
+    return sorted(sources, key=itemgetter('Provider'))
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,5 +1,48 @@
+import responses
+
 from skywatch_data_sources.sources import get_data_sources
 
 
-def test_get_data_sources():
-    get_data_sources()
+@responses.activate
+def test_get_data_sources_success(xml_data):
+    responses.add(
+        responses.GET,
+        'https://www.skywatch.com/sitemap.xml',
+        body=xml_data,
+        status=200
+    )
+
+    data_sources = get_data_sources()
+
+    expected_result = [
+        {
+            'Source': 'Sentinel-2',
+            'Provider': 'ESA',
+            'Resolution': '10m'
+        },
+        {
+            'Source': 'SuperView-1',
+            'Provider': 'SpaceView',
+            'Resolution': '50cm'
+        }
+    ]
+
+    assert data_sources == expected_result
+    assert len(responses.calls) == 1
+
+
+@responses.activate
+def test_get_data_sources_error(xml_data):
+    responses.add(
+        responses.GET,
+        'https://www.skywatch.com/sitemap.xml',
+        body='{error}$ 541',
+        status=404
+    )
+
+    data_sources = get_data_sources()
+
+    expected_result = []
+
+    assert data_sources == expected_result
+    assert len(responses.calls) == 1
